126449bf6a13da4854fd4c5215f42062fbecb8f3
rehan <rehanjutt005@gmail.com>
2022-07-11 17:56:48 +0500

    Initial commit

---
+++ lib/app/modules/category/controllers/categories_controller.dart
@@ -0,0 +1,44 @@
+import 'package:get/get.dart';
+
+import '../../../../common/ui.dart';
+import '../../../models/category_model.dart';
+import '../../../repositories/category_repository.dart';
+
+enum CategoriesLayout { GRID, LIST }
+
+class CategoriesController extends GetxController {
+  CategoryRepository _categoryRepository;
+
+  final categories = <Category>[].obs;
+  final layout = CategoriesLayout.LIST.obs;
+
+  CategoriesController() {
+    _categoryRepository = new CategoryRepository();
+  }
+
+  @override
+  Future<void> onInit() async {
+    await refreshCategories();
+    super.onInit();
+  }
+
+  Future refreshCategories({bool showMessage}) async {
+    await getCategories();
+    if (showMessage == true) {
+      Get.showSnackbar(Ui.SuccessSnackBar(message: "List of categories refreshed successfully".tr));
+    }
+  }
+
+  Future getCategories() async {
+    try {
+      categories.assignAll(await _categoryRepository.getAllWithSubCategories());
+      // categories.add(categories[categories.length-1]);
+      // categories[categories.length-1].name = "Travel App";
+      // categories[categories.length-1].description = "";
+      // categories[categories.length-1].image.url = "https://home-services.smartersvision.com/storage/app/public/369/car.svg";
+
+    } catch (e) {
+      Get.showSnackbar(Ui.ErrorSnackBar(message: e.toString()));
+    }
+  }
+}
---
+++ lib/app/modules/category/widgets/category_list_item_widget.dart
@@ -0,0 +1,113 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import '../../../../common/ui.dart';
+import '../../../../travel_app/pages/tabs.dart';
+import '../../../../travel_app/pages/welcome.dart';
+import '../../../models/category_model.dart';
+import '../../../routes/app_routes.dart';
+
+class CategoryListItemWidget extends StatelessWidget {
+  final Category category;
+  final String heroTag;
+  final bool expanded;
+  final bool travelAppWidget;
+
+  CategoryListItemWidget({Key key, this.category, this.heroTag, this.expanded, this.travelAppWidget = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print("image url: ${category.image.url}");
+    return Container(
+      padding: EdgeInsets.only(top: 8),
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      decoration: Ui.getBoxDecoration(
+          border: Border.fromBorderSide(BorderSide.none),
+          gradient: new LinearGradient(
+              colors: [category.color.withOpacity(0.6), category.color.withOpacity(0.1)],
+              begin: AlignmentDirectional.topStart,
+              //const FractionalOffset(1, 0),
+              end: AlignmentDirectional.topEnd,
+              stops: [0.0, 0.5],
+              tileMode: TileMode.clamp)),
+      child: Theme(
+        data: Get.theme.copyWith(dividerColor: Colors.transparent),
+        child: ExpansionTile(
+          initiallyExpanded: this.expanded,
+          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+          title: InkWell(
+              highlightColor: Colors.transparent,
+              splashColor: Get.theme.colorScheme.secondary.withOpacity(0.08),
+              onTap: () {
+                travelAppWidget ? Get.to(TabsExample()) :
+                Get.toNamed(Routes.CATEGORY, arguments: category);
+                //Navigator.of(context).pushNamed('/Details', arguments: RouteArgument(id: '0', param: market.id, heroTag: heroTag));
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: (category.image.url.toLowerCase().endsWith('.svg')
+                        ? SvgPicture.network(
+                            travelAppWidget ?
+                            "https://home-services.smartersvision.com/storage/app/public/369/car.svg" :
+                            category.image.url,
+                            color: category.color,
+                            height: 100,
+                          )
+                        : CachedNetworkImage(
+                            fit: BoxFit.cover,
+                            imageUrl: category.image.url,
+                            placeholder: (context, url) => Image.asset(
+                              'assets/img/loading.gif',
+                              fit: BoxFit.cover,
+                            ),
+                            errorWidget: (context, url, error) => Icon(Icons.error_outline),
+                          )),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      travelAppWidget ? "Travel Service" :
+                      category.name,
+                      overflow: TextOverflow.fade,
+                      softWrap: false,
+                      style: Get.textTheme.bodyText2,
+                    ),
+                  ),
+                  // TODO get service for each category
+                  // Text(
+                  //   "(" + category.services.length.toString() + ")",
+                  //   overflow: TextOverflow.fade,
+                  //   softWrap: false,
+                  //   style: Get.textTheme.caption,
+                  // ),
+                ],
+              )),
+          children: List.generate(category.subCategories?.length ?? 0, (index) {
+            var _category = category.subCategories.elementAt(index);
+            return GestureDetector(
+              onTap: () {
+                Get.toNamed(Routes.CATEGORY, arguments: _category);
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+                child: Text(_category.name, style: Get.textTheme.bodyText1),
+                decoration: BoxDecoration(
+                  color: Get.theme.scaffoldBackgroundColor.withOpacity(0.2),
+                  border: Border(top: BorderSide(color: Get.theme.scaffoldBackgroundColor.withOpacity(0.3))
+                      //color: Get.theme.focusColor.withOpacity(0.2),
+                      ),
+                ),
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
